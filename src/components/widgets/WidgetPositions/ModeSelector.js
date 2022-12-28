93ba8c24f5d321fb191022ced9cfa6b335e09eea
Salmane <salmane.hajouji@me.com>
2022-12-28 23:26:14 +0100

    Add: small check on mode selector

--- src/components/widgets/WidgetPositions/ModeSelector.js
+++ src/components/widgets/WidgetPositions/ModeSelector.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
+import axios from "axios";
+import { errorAtom } from "src/recoil/atoms";
+import { useSetRecoilState } from "recoil";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 
 import { MODES } from "./constants";
-import axios from "axios";
-import { errorAtom } from "src/recoil/atoms";
-import { useSetRecoilState } from "recoil";
 
 const REST_URL = "http://localhost:6969/setMode";
 
@@ -22,7 +22,9 @@ export default function ModeSelector({ currentMode, symbol, position_side }) {
         position_side,
       })
       .then((response) => {
-        setMode(event.target.value);
+        if (response?.status === 200) {
+          setMode(event.target.value);
+        }
       })
       .catch((err) => {
         setErrorRecoil(err);
@@ -32,7 +34,6 @@ export default function ModeSelector({ currentMode, symbol, position_side }) {
 
   return (
     <FormControl sx={{ minWidth: 125 }} size="small">
-      {/* <InputLabel id="demo-select-small">Set Mode</InputLabel> */}
       <Select value={MODES[mode]?.value} onChange={handleChange}>
         {Object.values(MODES).map((m) => (
           <MenuItem key={m.value} value={m.value}>
